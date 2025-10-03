8351e97de1610e9d3ad288124e56eb2488962f1b
User <User@DEVELOPERPC-00>
2025-10-03 20:18:09 +0200

    Add project files.

---
+++ FAI.Core/Entities/Movies/Genre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAI.Core.Entities.Movies
+{
+    public class Genre
+    {
+        public Genre() { }
+
+        //[Key, Column(Order = 0)] /* Column mit Order bei kombinierten Schlüssel-Attributen */
+        public virtual int Id { get; set; }
+        [MaxLength(60), MinLength(2)]
+        [Required]
+        public virtual string Name { get; set; } = string.Empty;
+    }
+}
---
+++ FAI.Core/Entities/Movies/MediumType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAI.Core.Entities.Movies
+{
+    // [Table(name: "XY")] Table Attribute, um den SQL-Tabellen Namen zu übersteuern
+    public class MediumType
+    {
+        public MediumType() { }
+
+        [MaxLength(8), MinLength(2)]
+        [Key]
+        public virtual string Code { get; set; }
+
+        [MaxLength(32),MinLength(2)]
+        [Required]
+        public virtual string Name { get; set; }
+    }
+}
